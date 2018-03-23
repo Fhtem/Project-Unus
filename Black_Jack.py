61d2ba655c2c0b35b29a663a658527a4c0432eab
Fhtem <36800218+Fhtem@users.noreply.github.com>
2018-03-23 13:00:09 -0500

    Update Black_Jack.py

--- Black_Jack.py
+++ Black_Jack.py
@@ -1,10 +1,25 @@
 import random
 
-deck = {}
+init_deck = {}
 
 for number in range(1, 14):
-    for suit in ['spades', 'clubs', 'diamonds', 'hearts']
-        if number <= 10:
-            deck[str(number) + 'S'] = number
-        else:
-            deck[str(number) + '']
+	for suit in ['spades', 'clubs', 'diamonds', 'hearts']:
+		if number == 1:
+			key = 'ace of %s' % (suit)
+			deck[key] = 1
+		elif number <= 10:
+			key = '%d of %s' % (number, suit)
+			deck[key] = number
+		elif number == 11:
+			key = 'jack of %s' % (suit)
+			deck[key] = 10
+		elif number == 12:
+			key = 'queen of %s' % (suit)
+			deck[key] = 10
+		elif number == 13:
+			key = 'king of %s' % (suit)
+			deck[key] = 10
+deck = init_deck
+
+print (len(deck))
+print (keys(deck))
